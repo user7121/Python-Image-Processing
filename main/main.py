967b83359a939580ddcd1e5e520e58b165860c68
Emir Şeker <158557458+user7121@users.noreply.github.com>
2026-02-21 14:20:10 +0300

    Update main.py

--- main/main.py
+++ main/main.py
@@ -2,8 +2,8 @@ from pathlib import Path
 from PIL import Image, ImageFilter
 import matplotlib.pyplot as plt
 
-HERE = Path(__file__).resolve().parent
-img = Image.open(HERE / "image" / "input.png")
+ROOT = Path(__file__).resolve().parent.parent
+img = Image.open(ROOT / "image" / "input.png")
 
 rot_45 = img.rotate(45, expand=True)
 resized = img.resize((400, 400))
